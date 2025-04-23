65d740f943fdd45fefd99e3e081e9e267188a726
Garbonzo24 <blake.caldwell@geappliances.com>
2025-04-23 16:15:33 -0400

    Update Customer_info.sql

--- Customer_info.sql
+++ Customer_info.sql
@@ -1,7 +1,8 @@
 SELECT
     ca.cust_account_id,
     ca.account_number,
-    p.party_name,
+    customer.party_name AS customer_name,
+    contact.party_name AS contact_name,
     cp.contact_point_type,
     cp.email_address,
     cp.phone_number,
@@ -12,11 +13,16 @@ FROM
 JOIN 
     AR.HZ_CUST_ACCOUNTS ca ON prof.cust_account_id = ca.cust_account_id
 JOIN 
-    AR.HZ_PARTIES p ON ca.party_id = p.party_id
+    AR.HZ_PARTIES customer ON ca.party_id = customer.party_id
 JOIN 
-    AR.HZ_CONTACT_POINTS cp ON cp.owner_table_id = p.party_id
+    AR.HZ_RELATIONSHIPS rel
+        ON rel.object_id = customer.party_id OR rel.subject_id = customer.party_id
+JOIN 
+    AR.HZ_CONTACT_POINTS cp 
+        ON cp.owner_table_id = rel.party_id
+LEFT JOIN 
+    AR.HZ_PARTIES contact ON rel.subject_id = contact.party_id OR rel.object_id = contact.party_id
 WHERE 
-    prof.collector_id = 256022 -- Needs to be collector id found in AR_COLLECTORS
+    prof.collector_id = 256022 -- Needs to be collector ID found in oracle
     AND cp.contact_point_type IN ('EMAIL', 'PHONE')
     AND cp.status = 'A'
-    AND p.party_name IS NOT NULL
